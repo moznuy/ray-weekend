cca0ed29797c2f0eb799a9acb0768377a25da587
Serhii Charykov <laammaar@gmail.com>
2025-03-02 17:35:00 +0200

    Shooting rays

--- src/main.zig
+++ src/main.zig
@@ -6,10 +6,12 @@ pub const Vec3 = struct {
 
     const Self = @This();
 
-    pub fn init(fields: [3]f64) Self {
-        return .{
-            .e = fields,
-        };
+    pub inline fn initA(fields: [3]f64) Self {
+        return .{ .e = fields };
+    }
+
+    pub inline fn initN(_x: f64, _y: f64, _z: f64) Self {
+        return .{ .e = .{ _x, _y, _z } };
     }
 
     pub inline fn x(self: Self) f64 {
@@ -32,8 +34,8 @@ pub const Vec3 = struct {
         return .{ .e = [_]f64{ -self.e[0], -self.e[1], -self.e[2] } };
     }
 
-    pub fn scale(self: Self, scalar: anytype) Self {
-        return .{ .e = [_]f64{ self.e[0] * scalar, -self.e[1] * scalar, -self.e[2] * scalar } };
+    pub fn scale(self: Self, scalar: f64) Self {
+        return .{ .e = [_]f64{ self.e[0] * scalar, self.e[1] * scalar, self.e[2] * scalar } };
     }
 
     pub fn length(self: Self) f64 {
@@ -101,6 +103,15 @@ pub inline fn set_color(data: []u8, color: Color3, i: usize, j: usize, comptime 
     data[(i * image_width + j) * num_components + 2] = bb;
 }
 
+pub fn ray_color(ray: Ray3) Color3 {
+    const unit_direction = ray.dir.unit();
+    const a = 0.5 * (unit_direction.y() + 1.0);
+
+    const white = Color3.initN(1.0, 1.0, 1.0);
+    const blue = Color3.initN(0.5, 0.7, 1.0);
+    return white.scale(1.0 - a).add(blue.scale(a));
+}
+
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer {
@@ -111,18 +122,39 @@ pub fn main() !void {
     zstbi.init(gpa.allocator());
     defer zstbi.deinit();
 
-    const image_width = 256;
-    const image_height = 256;
-    const num_components = 3;
-    const bytes_per_component = @sizeOf(u8);
+    // Image
+    const aspect_ration: comptime_float = 16.0 / 9.0;
+    const image_width: comptime_int = 400;
+    const image_height_tmp: comptime_int = @intFromFloat(@as(comptime_float, @floatFromInt(image_width)) / aspect_ration);
+    const image_height: comptime_int = if (image_height_tmp < 0) 1 else image_height_tmp;
 
+    // Camera
+    const focal_length: comptime_float = 1.0;
+    const viewport_height: comptime_float = 2.0;
+    const viewport_width: comptime_float = viewport_height * (@as(comptime_float, @floatFromInt(image_width)) / @as(comptime_float, @floatFromInt(image_height)));
+    const camera_center = Point3.initN(0, 0, 0);
+    const viewport_u = Vec3.initN(viewport_width, 0, 0);
+    const viewport_v = Vec3.initN(0, -viewport_height, 0);
+    const pixel_delta_u = viewport_u.scale(1.0 / @as(f64, @floatFromInt(image_width)));
+    const pixel_delta_v = viewport_v.scale(1.0 / @as(f64, @floatFromInt(image_height)));
+    const viewport_upper_left = camera_center.sub(Vec3.initN(0, 0, focal_length)).sub(viewport_u.scale(0.5)).sub(viewport_v.scale(0.5));
+    const pixel00_loc = viewport_upper_left.add(pixel_delta_u.add(pixel_delta_v).scale(0.5));
+
+    std.debug.print("{} {}\n", .{ viewport_upper_left, pixel00_loc });
+
+    // Data
+    const num_components: comptime_int = 3;
+    const bytes_per_component = @sizeOf(u8);
     var data: [image_width * image_height * num_components]u8 = undefined;
     @memset(&data, 0);
 
     for (0..image_height) |i| {
         std.debug.print("\rScanlines remaining: {d:04}", .{image_height - i});
         for (0..image_width) |j| {
-            const color = Color3.init(.{ @as(f64, @floatFromInt(j)) / (image_width - 1), @as(f64, @floatFromInt(i)) / (image_height - 1), 0 });
+            // const color = Color3.initN(@as(f64, @floatFromInt(j)) / (image_width - 1), @as(f64, @floatFromInt(i)) / (image_height - 1), 0);
+            const pixel_center = pixel00_loc.add(pixel_delta_u.scale(@floatFromInt(j))).add(pixel_delta_v.scale(@floatFromInt(i)));
+            const ray_direction = pixel_center.sub(camera_center);
+            const color = ray_color(.{ .orig = camera_center, .dir = ray_direction });
             set_color(&data, color, i, j, image_width, num_components);
         }
     }
