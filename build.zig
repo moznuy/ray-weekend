b324fcb8e13ba9579c5aab0bffb16ccf78c6e503
Serhii Charykov <laammaar@gmail.com>
2025-03-04 05:07:51 +0200

    Revert zig to stable version 0.13

--- build.zig
+++ build.zig
@@ -3,14 +3,11 @@ const std = @import("std");
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
-    const exe_mod = b.createModule(.{
+    const exe = b.addExecutable(.{
+        .name = "ray-zig1",
         .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
-    });
-    const exe = b.addExecutable(.{
-        .name = "ray-zig1",
-        .root_module = exe_mod,
     });
     const zstbi = b.dependency("zstbi", .{});
     exe.root_module.addImport("zstbi", zstbi.module("root"));
