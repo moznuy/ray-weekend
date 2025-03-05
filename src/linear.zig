ec6441c716939d83439867d8ed651405f84abfbd
Serhii Charykov <laammaar@gmail.com>
2025-03-05 21:55:24 +0200

    Move from array of floats to @Vector of floats for simd

--- src/linear.zig
+++ src/linear.zig
@@ -1,16 +1,18 @@
 const std = @import("std");
 
+const VecType = @Vector(4, f64);
+
 pub const Vec3 = struct {
-    e: [3]f64 = [_]f64{ 0, 0, 0 },
+    e: VecType,
 
     const Self = @This();
 
     pub inline fn initA(fields: [3]f64) Self {
-        return .{ .e = fields };
+        return .{ .e = .{ fields[0], fields[1], fields[2], 0 } };
     }
 
     pub inline fn initN(_x: f64, _y: f64, _z: f64) Self {
-        return .{ .e = .{ _x, _y, _z } };
+        return .{ .e = .{ _x, _y, _z, 0 } };
     }
 
     pub inline fn x(self: Self) f64 {
@@ -22,78 +24,60 @@ pub const Vec3 = struct {
     pub inline fn z(self: Self) f64 {
         return self.e[2];
     }
-
-    pub fn accumulate(self: *Self, other: Self) void {
-        self.e[0] += other.e[0];
-        self.e[1] += other.e[1];
-        self.e[2] += other.e[2];
+    pub inline fn w(self: Self) f64 {
+        return self.e[3];
     }
 
-    pub fn negate(self: Self) Self {
-        return .{ .e = [_]f64{
-            -self.e[0],
-            -self.e[1],
-            -self.e[2],
-        } };
+    pub inline fn accumulate(self: *Self, other: Self) void {
+        self.e += other.e;
     }
 
-    pub fn scale(self: Self, scalar: f64) Self {
-        return .{ .e = [_]f64{
-            self.e[0] * scalar,
-            self.e[1] * scalar,
-            self.e[2] * scalar,
-        } };
+    pub inline fn negate(self: Self) Self {
+        return .{ .e = -self.e };
     }
 
-    pub fn length(self: Self) f64 {
+    pub inline fn scale(self: Self, scalar: f64) Self {
+        return .{ .e = self.e * @as(VecType, @splat(scalar)) };
+    }
+
+    pub inline fn length(self: Self) f64 {
         return @sqrt(self.length_squred());
     }
 
-    pub fn length_squred(self: Self) f64 {
-        return self.e[0] * self.e[0] + self.e[1] * self.e[1] + self.e[2] * self.e[2];
+    pub inline fn length_squred(self: Self) f64 {
+        return @reduce(.Add, self.mul(self).e);
     }
 
-    pub fn add(self: Self, other: Self) Self {
-        return .{ .e = [_]f64{
-            self.e[0] + other.e[0],
-            self.e[1] + other.e[1],
-            self.e[2] + other.e[2],
-        } };
+    pub inline fn add(self: Self, other: Self) Self {
+        return .{ .e = self.e + other.e };
     }
 
-    pub fn sub(self: Self, other: Self) Self {
-        return .{ .e = [_]f64{
-            self.e[0] - other.e[0],
-            self.e[1] - other.e[1],
-            self.e[2] - other.e[2],
-        } };
+    pub inline fn sub(self: Self, other: Self) Self {
+        return .{ .e = self.e - other.e };
     }
 
-    pub fn mul(self: Self, other: Self) Self {
-        return .{ .e = [_]f64{
-            self.e[0] * other.e[0],
-            self.e[1] * other.e[1],
-            self.e[2] * other.e[2],
-        } };
+    pub inline fn mul(self: Self, other: Self) Self {
+        return .{ .e = self.e * other.e };
     }
 
-    pub fn dot(self: Self, other: Self) f64 {
-        return self.e[0] * other.e[0] + self.e[1] * other.e[1] + self.e[2] * other.e[2];
+    pub inline fn dot(self: Self, other: Self) f64 {
+        return @reduce(.Add, self.mul(other).e);
     }
 
-    pub fn cross(self: Self, other: Self) Self {
+    pub inline fn cross(self: Self, other: Self) Self {
         return .{ .e = [_]f64{
             self.e[1] * other.e[2] - self.e[2] * other.e[1],
             self.e[2] * other.e[0] - self.e[0] * other.e[2],
             self.e[0] * other.e[1] - self.e[1] * other.e[0],
+            0,
         } };
     }
 
-    pub fn reflect(self: Self, normal: Self) Self {
+    pub inline fn reflect(self: Self, normal: Self) Self {
         return self.sub(normal.scale(2 * self.dot(normal)));
     }
 
-    pub fn refract(self: Self, normal: Self, etai_over_etat: f64) Self {
+    pub inline fn refract(self: Self, normal: Self, etai_over_etat: f64) Self {
         const cos_theta = @min(self.negate().dot(normal), 1.0);
         const ray_out_perp = self.add(normal.scale(cos_theta)).scale(etai_over_etat);
         // TODO: try 1-@abs(x) instead of @abs(1-x)
@@ -101,33 +85,35 @@ pub const Vec3 = struct {
         return ray_out_perp.add(ray_out_parallel);
     }
 
-    pub fn unit(self: Self) Self {
+    pub inline fn unit(self: Self) Self {
         return self.scale(1 / self.length());
     }
 
-    pub fn near_zero(self: Self) bool {
+    pub inline fn near_zero(self: Self) bool {
         const s = 1e-8;
         return @abs(self.e[0]) < s and @abs(self.e[1]) < s and @abs(self.e[2]) < s;
     }
 };
 
-pub fn random_vec(rand: std.Random) Vec3 {
-    return .{ .e = [_]f64{
+pub inline fn random_vec(rand: std.Random) Vec3 {
+    return .{ .e = .{
         rand.float(f64),
         rand.float(f64),
         rand.float(f64),
+        0,
     } };
 }
 
-pub fn random_vec_range(rand: std.Random, min: f64, max: f64) Vec3 {
-    return .{ .e = [_]f64{
+pub inline fn random_vec_range(rand: std.Random, min: f64, max: f64) Vec3 {
+    return .{ .e = .{
         random_double_range(rand, min, max),
         random_double_range(rand, min, max),
         random_double_range(rand, min, max),
+        0,
     } };
 }
 
-pub fn random_unit_vector(rand: std.Random) Vec3 {
+pub inline fn random_unit_vector(rand: std.Random) Vec3 {
     while (true) {
         const p = random_vec_range(rand, -1, 1);
         const lensq = p.length_squred();
@@ -137,7 +123,7 @@ pub fn random_unit_vector(rand: std.Random) Vec3 {
     }
 }
 
-pub fn random_on_hemisphere(rand: std.Random, normal: Vec3) Vec3 {
+pub inline fn random_on_hemisphere(rand: std.Random, normal: Vec3) Vec3 {
     const on_unit_sphere = random_unit_vector(rand);
     if (on_unit_sphere.dot(normal) > 0.0) {
         return on_unit_sphere;
@@ -146,13 +132,15 @@ pub fn random_on_hemisphere(rand: std.Random, normal: Vec3) Vec3 {
     }
 }
 
-pub fn random_in_unit_disk(rand: std.Random) Vec3 {
+pub inline fn random_in_unit_disk(rand: std.Random) Vec3 {
     while (true) {
-        const p = Vec3.initN(
+        const p = Vec3{ .e = .{
             random_double_range(rand, -1, 1),
             random_double_range(rand, -1, 1),
             0,
-        );
+            0,
+        } };
+
         if (p.length_squred() < 1) {
             return p;
         }
